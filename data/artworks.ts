e8f6c13eec93fa9ffbb91b3c201a0a61065d31ef
Zertx framework <zertx20@gmail.com>
2025-12-31 04:41:16 +0100

    Initial commit: Art Gallery Slider with clean image display

---
+++ data/artworks.ts
@@ -0,0 +1,46 @@
+import type { Artwork } from "@/types/artwork"
+
+export const artworks: Artwork[] = [
+  {
+    id: 1,
+    title: "",
+    artist: "Marina Solace",
+    image: "/IMG_6842.JPG",
+  },
+  {
+    id: 2,
+    title: "",
+    artist: "Chen Wei",
+    image: "/IMG_2792.JPG",
+  },
+  {
+    id: 3,
+    title: "",
+    artist: "Elise Beaumont",
+    image: "/IMG_0596.JPG",
+  },
+  {
+    id: 4,
+    title: "",
+    artist: "Yuki Tanaka",
+    image: "/IMG_1581.JPG",
+  },
+  {
+    id: 6,
+    title: "",
+    artist: "Alex Reiner",
+    image: "/IMG_7360.JPG",
+  },
+  {
+    id: 7,
+    title: "",
+    artist: "Lila Fontaine",
+    image: "/IMG_4741.JPG",
+  },
+  {
+    id: 8,
+    title: "",
+    artist: "David Kross",
+    image: "/IMG_7298 (2).JPG",
+  },
+]
